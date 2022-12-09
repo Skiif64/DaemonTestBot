0cd54b170224fc15d1a0d62fc0c0757eda22acbd
Skiif64 <skiif64@yandex.ru>
2022-12-09 23:09:13 +0400

    Добавьте файлы проекта.

---
+++ DaemonTestBot/BotSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace DaemonTestBot;
+
+public class BotSettings
+{
+    public string BotToken { get; init; } = string.Empty;
+    public long ChatId { get; init; }
+}
---
+++ DaemonTestBot/TelegramBot.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace DaemonTestBot
+{
+    public class TelegramBot : IHostedService
+    {
+        private readonly ITelegramBotClient _client;
+        private readonly ILogger<TelegramBot> _logger;
+        private readonly BotSettings _settings;
+        public TelegramBot(IOptions<BotSettings> settings, ILogger<TelegramBot> logger)
+        {
+            _settings = settings.Value;
+            _client = new TelegramBotClient(_settings.BotToken);
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var recieverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+                ThrowPendingUpdates = true
+            };
+            _client.StartReceiving(
+                updateHandler: HandleUpdate,
+                receiverOptions: recieverOptions,
+                pollingErrorHandler: HandleError,
+                cancellationToken: cancellationToken);
+            var me = await _client.GetMeAsync(cancellationToken);
+            _logger.LogInformation($"Bot @{me.Username} started listening.");
+        }
+
+        private async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type != UpdateType.Message)
+                return;
+
+            var message = update.Message!;
+            var messageText = message.Text!;
+
+            var handler = messageText switch
+            {
+                "/start" => OnStartMessageRecieved(_settings.ChatId, cancellationToken),
+                "Статус" => OnStatusMessageRecieved(_settings.ChatId, cancellationToken),
+                _ => OnStatusMessageRecieved(_settings.ChatId, cancellationToken)
+            };
+            await handler;
+        }
+
+        private async Task OnStartMessageRecieved(long chatId, CancellationToken cancellationToken)
+        {
+            var text = "Привет! Это бот для проеверки systemd и того, как он будет отрабатывать длительное время";
+            var keyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton("Статус"));
+            await _client.SendTextMessageAsync(
+                chatId: chatId,
+                text: text,
+                replyMarkup: keyboardMarkup,
+                cancellationToken: cancellationToken
+                );
+        }
+
+        private Task OnStatusMessageRecieved(long chatId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task HandleError(ITelegramBotClient client, Exception exception, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
